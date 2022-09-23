62779734d679d0571fded2c1b31383047e4e0f15
VitaPetrushyna <masandra2018@ukr.net>
2022-09-23 13:24:54 +0300

    add propTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,5 @@
+// import PropTypes from 'prop-types';
+
 export const Profile = ({
   user: {
     username,
@@ -33,3 +35,7 @@ export const Profile = ({
     </div>
   );
 };
+
+// Profile.propTypes = {
+//   user,
+// };
